0c3e8273c4bbdd2ae02d67317a5b5959424de263
bader <https://github.com/baderkj>
2025-02-02 16:16:08 +0300

    new 2025

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
         scroll(0, 0);
     };
 
-    window.addEventListener('scroll', function ll(argument) {
+    window.addEventListener('scroll', function (argument) {
         if (scrollY > 400) {
             arrow.classList.remove('hide');
         } else {
@@ -13,3 +13,15 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+// script.js
+document.querySelector('.cv-button').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default link behavior
+    const confirmDownload = confirm('Are you sure you want to download my CV?');
+    
+    if (confirmDownload) {
+      window.location.href = this.href; // Proceed with the download
+      alert('Thank you for downloading my CV!');
+    } else {
+      alert('Download canceled.');
+    }
+  });
